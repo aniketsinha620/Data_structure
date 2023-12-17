8fb0cac00220a007594ac4d943b59a707932af53
aniketsinha620 <aniketsinha620@gmail.com>
2023-12-17 22:25:12 +0530

    added in sorting

--- practice.cpp
+++ practice.cpp
@@ -4,37 +4,9 @@
 
 using namespace std;
 
-void form(set<vector<string>> &s, vector<string> &dp, int n, int N)
-{
-    if (n == N)
-    {
-        s.insert(dp);
 
-        return;
-    }
 
-    dp.push_back("0"); 
-    form(s, dp, n + 1, N);
-    if (n == 0 || dp[n - 1] == "0") 
-    {
-        dp.pop_back();
-        dp.push_back("1"); 
-        form(s, dp, n + 1, N);
-    }
-    dp.pop_back();
-}
 
-vector<string> generateString(int n)
-{
-    set<vector<string>> s;
-    vector<string> dp;
-    form(s, dp, 0, n);
-    vector<vector<string>> arr;
-    for (const auto &vec : s)
-    {
-        arr.push_back(vec);
-    }
-}
 
 int main()
 {
